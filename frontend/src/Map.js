c7781bcf314789e931a3865deb3e75bbec215ffe
copperstick6 <copperstick6@gmail.com>
2019-04-24 01:53:39 -0500

    added map and animations

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -14,20 +14,26 @@ export default class Map extends Component {
         this.typingEnd = this.typingEnd.bind(this)
     }
     typingEnd(){
-        console.log("entered")
-        this.setState({typingEnd: false})
+        setTimeout(function(){
+            this.setState({typingEnd: false})
+        }.bind(this), 1000)
+
     }
     render(){
+        let done = null
+        if(this.state.done){
+            done = <div><h5>HI</h5></div>
+        }
         return (
           <div className="Intro">
-          <Fade when={this.state.typingEnd} duration={3000} collapse onReveal={()=> this.setState({done: true})}>
+          <Fade when={this.state.typingEnd} duration={3000} wait={4000} onReveal={()=> this.setState({done: true})}>
       		<div className="container">
       		<TypeWriter typing={1} onTypingEnd={this.typingEnd}><h1>Welcome to Marauder's Map</h1>
       		<h2>GDC edition</h2>
-
       		</TypeWriter>
       		</div>
           </Fade>
+          {done}
           </div>
         )
     }
