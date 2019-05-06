ececa4ea7fd95e5a3134bc8f9fac2ff3b1d3bc6c
copperstick6 <copperstick6@gmail.com>
2019-05-06 16:29:48 -0500

    created numpy elements, starting on nn loaders

--- nn/cleaner.py
+++ nn/cleaner.py
@@ -1,9 +1,20 @@
-
+import numpy as np
 
 
 datasets = ["2216.txt", "43024304.txt", "atrium.txt", "lab.txt"]
+final_arr = []
 
-for i in datasets:
-    f = open("datasets/"+i, "r")
-    contents = f.read()
-    print(contents)
+for i in range(0, len(datasets)):
+    f = open("datasets/"+datasets[i], "r")
+    for line in f:
+        line = line[:-1]
+        line_item = line.split(" ")
+        line_item = ["0" if x=="XXX" else x for x in line_item]
+        line_item[2].replace("\n", "")
+        line_item = [0 if x =="0" else int(x) + 100 for x in line_item]
+        line_item.append(i + 1)
+        final_arr.append(line_item)
+numpy_arr = np.array(final_arr)
+print(numpy_arr)
+
+np.save("datasets/numpy_data", numpy_arr)
