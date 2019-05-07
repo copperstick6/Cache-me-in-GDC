35664a0f8520ddbbac721b6ead608a69900fa901
copperstick6 <copperstick6@gmail.com>
2019-05-07 17:58:32 -0500

    added server

--- backend/server.py
+++ backend/server.py
@@ -4,24 +4,24 @@ from flask import Flask, request, render_template, redirect
 app = Flask(__name__)
 import torch
 from torch import nn, optim
+import json
 
 from models import *
 
 
 @app.route("/")
 def evaluateModel():
-    power1 = request.args.get("power_1")
-    power2 = request.args.get("power_2")
-    power3 = request.args.get("power_3")
-    batch_inputs = torch.Tensor(np.array([[int(power1), int(power2), int(power3)]]))
-
+    data = request.args.get("json")
+    data = json.loads(data)
     model = ConvNetModel()
     pred = lambda x: np.argmax(x.detach().numpy(), axis=1)
     model.load_state_dict(torch.load('deep'))
     model.eval()
-    pred_val = torch.Tensor(pred(model(batch_inputs)))
-    print(int(pred_val.data.numpy().tolist()[0]))
-    return str(int(pred_val.data.numpy().tolist()[0]))
+    for i in data:
+        batch_inputs = torch.Tensor(np.array([[int(i["power1"]), int(i["power2"]), int(i["power3"])]]))
+        pred_val = torch.Tensor(pred(model(batch_inputs)))
+        i["room"] = str(int(pred_val.data.numpy().tolist()[0]))
+    return json.dumps(data)
 
 
 if __name__ == '__main__':
